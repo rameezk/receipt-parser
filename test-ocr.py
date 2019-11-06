2aa5bb2946f17de8b68ed2977865b34728075d2b
Rameez Khan <rameezkhan.sa@gmail.com>
2019-11-06 07:44:09 +0200

    Convert PDF's to images

--- test-ocr.py
+++ test-ocr.py
@@ -1,13 +1,20 @@
 import pytesseract
 import PIL.Image
 import sys
+from wand.image import Image as Img
 
 
-def process(file):
+def process_image(file):
     print("[..] Processing...")
     image = PIL.Image.open(file)
     text = pytesseract.image_to_string(image)
     return {"text": text}
+
+
+def process_pdf(pdf):
+    with Img(filename=pdf, resolution=300) as img:
+        img.compression_quality = 99
+        img.save(filename=f"{pdf}.jpg")
 
 
 if len(sys.argv) < 2:
@@ -15,4 +22,9 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 file_path = sys.argv[1]
-print(process(file_path))
+
+if file_path.lower().endswith(".pdf"):
+    process_pdf(file_path)
+    process_image(f"{file_path}.jpg")
+else:
+    print(process_image(file_path))
